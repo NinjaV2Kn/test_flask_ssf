15af2435579a851185d2315ead6bcd29b2a316f4
NinjaV2K <144907470+NinjaV2Kn@users.noreply.github.com>
2023-11-14 14:17:27 +0100

    Update BottleSensors.py
    Setup the 4 Sensors and added a little delay

--- BottleSensors.py
+++ BottleSensors.py
@@ -8,6 +8,9 @@ sensor4 = 22
 
 GPIO.setmode(GPIO.BCM) #Set GPIO pin numbering
 GPIO.setup(sensor1, GPIO.IN) #Sensor 1
+GPIO.setup(sensor2, GPIO.IN) #Sensor 2
+GPIO.setup(sensor3, GPIO.IN) #Sensor 3
+GPIO.setup(sensor4, GPIO.IN) #Sensor 4
 
 def button_state(input_pin: int): # returns the value of the pin
     if GPIO.input(input_pin): 
@@ -18,9 +21,13 @@ def button_state(input_pin: int): # returns the value of the pin
 while True:
     if button_state(sensor1) == "occupied":
         print("Sensor 1: Occupied")
+        time.sleep(0.5)
     elif button_state(sensor2) == "occupied":
         print("Sensor 2: Occupied")
+        time.sleep(0.5)
     elif button_state(sensor3) == "occupied":
         print("Sensor 3: Occupied")
+        time.sleep(0.5)
     elif button_state(sensor4) == "occupied":
         print("Sensor 4: Occupied")
+        time.sleep(0.5)
