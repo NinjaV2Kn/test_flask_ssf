4978e9714996075cfa51d0ca386e4a56e660a75d
NinjaV2K <144907470+NinjaV2Kn@users.noreply.github.com>
2023-11-23 12:46:12 +0100

    added an eventhandler

--- Main.py
+++ Main.py
@@ -1,8 +1,8 @@
 import BottleSensors as bs
+import asyncio
 import Nanoleaf as nls
-import time
 
-def main():
+async def main() -> None:
     """main function."""
     try:
         bs.setup_GPIO()
@@ -10,12 +10,16 @@ def main():
         print("setup complete")
         print("press CTRL+C to exit")
 
+        bottle_amount_old = 0
+
         while True:
-            bs.bottle_counter()
+            bottle_amount = bs.bottle_counter()
+            if bottle_amount != bottle_amount_old: #check if the bottle amount has changed
+                bs.bottle_counter()
+                nls.nanoleaf_indicator()
 
-            time.sleep(0.1)
-
-            nls.nanoleaf_indicator()
+                bottle_amount_old = bottle_amount
+                await asyncio.sleep(2) #wait 2 seconds to not overload the nanoleafes
 
     except KeyboardInterrupt:
         bs.GPIO.cleanup()
