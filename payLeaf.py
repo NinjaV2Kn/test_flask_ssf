3f9dd601353720241258c7b15362a04e264aefef
NinjaV2K <144907470+NinjaV2Kn@users.noreply.github.com>
2023-11-29 11:47:11 +0100

    simplyfied the funktion

--- payLeaf.py
+++ payLeaf.py
@@ -8,21 +8,13 @@ def payLeaf() -> None:
     """Lets the nanoleafes blink"""
     pause = 1/5
     current_effect = nlp.get_current_effect()
-    nlp_twin.set_all_colors((248, 222, 126))
-    nlp_twin.sync()
-    time.sleep(pause)
-    nlp_twin.set_all_colors((0, 0, 0))
-    nlp_twin.sync()
-    time.sleep(pause)
-    nlp_twin.set_all_colors((248, 222, 126))
-    nlp_twin.sync()
-    time.sleep(pause)
-    nlp_twin.set_all_colors((0, 0, 0))
-    nlp_twin.sync()
-    time.sleep(pause)
-    nlp_twin.set_all_colors((248, 222, 126))
-    nlp_twin.sync()
-    
+    for _ in range(3):
+        nlp_twin.set_all_colors((248, 222, 126))
+        nlp_twin.sync()
+        time.sleep(pause)
+        nlp_twin.set_all_colors((0, 0, 0))
+        nlp_twin.sync()
+        time.sleep(pause)    
     nlp.set_effect(current_effect)
 
 
@@ -30,18 +22,10 @@ def payLeaf2():
     pause = 1/3
     current_effect = nlp.get_current_effect()
     nlp.set_color((248, 222, 126))
-    nlp.set_brightness(100)
-    time.sleep(pause)
-    nlp.set_brightness(0)
-    time.sleep(pause)
-    nlp.set_brightness(100)
-    time.sleep(pause)
-    nlp.set_brightness(0)
-    time.sleep(pause)
-    nlp.set_brightness(100)
-    time.sleep(pause)
-    nlp.set_brightness(0)
-    time.sleep(pause)
-    nlp.set_brightness(100)
+    for _ in range(3):
+        nlp.set_brightness(100)
+        time.sleep(pause)
+        nlp.set_brightness(0)
+        time.sleep(pause)
     nlp.set_effect(current_effect)
     nlp.set_brightness(50)
