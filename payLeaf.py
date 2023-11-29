7b0a91b70efefea4c8c7b4d1e7c72ced39f1ebf6
Moeller <VW6UH2L@vw.vwgroup.com>
2023-11-29 13:30:30 +0100

    changed the color of the effect

--- payLeaf.py
+++ payLeaf.py
@@ -9,7 +9,7 @@ def payLeaf() -> None:
     pause = 1/5
     current_effect = nlp.get_current_effect()
     for _ in range(3):
-        nlp_twin.set_all_colors((248, 222, 126))
+        nlp_twin.set_all_colors((255, 100, 0))
         nlp_twin.sync()
         time.sleep(pause)
         nlp_twin.set_all_colors((0, 0, 0))
@@ -21,11 +21,14 @@ def payLeaf() -> None:
 def payLeaf2():
     pause = 1/3
     current_effect = nlp.get_current_effect()
-    nlp.set_color((248, 222, 126))
-    for _ in range(3):
-        nlp.set_brightness(100)
+    nlp.set_color((217, 142, 27))
+    for _ in range(2):
+        nlp.set_brightness(60)
         time.sleep(pause)
         nlp.set_brightness(0)
         time.sleep(pause)
     nlp.set_effect(current_effect)
     nlp.set_brightness(50)
+
+if __name__ == "__main__":
+    payLeaf2()
