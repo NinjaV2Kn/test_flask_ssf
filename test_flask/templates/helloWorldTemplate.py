a339318ac03acb4042e7553525a5fdb7cc5e1239
NinjaV2K <144907470+NinjaV2Kn@users.noreply.github.com>
2023-12-05 14:14:50 +0100

    added html

--- test_flask/templates/helloWorldTemplate.py
+++ test_flask/templates/helloWorldTemplate.py
@@ -24,4 +24,4 @@ def index():
    return render_template('index.html', **templateData)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='192.168.30.154', port=80, debug=True)
