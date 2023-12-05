9b08e5d854175122e780de129f0b5bb5280e2162
TheItalian <144907629+TheItaliann@users.noreply.github.com>
2023-12-05 15:24:49 +0100

    Update helloWorldTemplate.py

--- test_flask/templates/helloWorldTemplate.py
+++ test_flask/templates/helloWorldTemplate.py
@@ -1,22 +1,23 @@
-from flask import Flask, render_template
 import RPi.GPIO as GPIO
+from flask import Flask, render_template
 
-app = Flask(__name__)  # Use __name__ instead of __SSF__
-
-sensor1 = 21  # Define sensor1, replace 21 with your actual GPIO pin number
+app = Flask(__name__)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(sensor1, GPIO.IN)
+GPIO.setwarnings(False)
+sensor = 4
 
-@app.route('/')
+
+GPIO.setup(sensor, GPIO.IN)
+
+@app.route("/")
 def index():
-   # Read Sensors Status
-   sensorSts = GPIO.input(sensor1)
-
-   templateData = {
-      'title' : 'GPIO input Status!',
-      'button'  : sensorSts,
-   }
-   return render_template('index.html', **templateData)
+    sensorSts = GPIO.input(sensor)
+    templateData = {
+        'title': 'GPIO input Status!',
+        'button': sensorSts,
+    }
+    
+    return render_template('index.html', **templateData)
 
 if __name__ == "__main__":
-   app.run(host='192.168.30.154', port=80, debug=True)
+    app.run(host='192.168.30.154', port=80, debug=True)
