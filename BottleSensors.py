a8c7d7d447db7f0a9ce776ad605529299c673ff3
NinjaV2K <144907470+NinjaV2Kn@users.noreply.github.com>
2023-12-05 13:16:43 +0100

    made the code shorter

--- BottleSensors.py
+++ BottleSensors.py
@@ -4,37 +4,18 @@ import Nanoleaf as nls
 import json
 
 # each sensor is a GPIO pin so it can be diffrent(in this case buttons where used).
-try:
-    with open("config.json", "r") as file:
-        config = json.load(file)
-    sensors = {}
-    for x in range(1, 17):
-        sensors[f"sensor{x}"] = config[f"sensor{x}"]
-        print(sensors[f"sensor{x}"])
-except Exception as e:
-    print(e)
-
 def setup_GPIO() -> None:
     """setup the GPIO pins."""
     try:
         GPIO.setwarnings(False) #Disable warnings
         GPIO.setmode(GPIO.BCM) #Set GPIO pin numbering
-        GPIO.setup(sensor1, GPIO.IN) #Sensor 1
-        GPIO.setup(sensor2, GPIO.IN) #Sensor 2
-        GPIO.setup(sensor3, GPIO.IN) #Sensor 3
-        GPIO.setup(sensor4, GPIO.IN) #Sensor 4
-        #GPIO.setup(sensor5, GPIO.IN) #Sensor 5
-        #GPIO.setup(sensor6, GPIO.IN) #Sensor 6
-        #GPIO.setup(sensor7, GPIO.IN) #Sensor 7
-        #GPIO.setup(sensor8, GPIO.IN) #Sensor 8
-        #GPIO.setup(sensor9, GPIO.IN) #Sensor 9
-        #GPIO.setup(sensor10, GPIO.IN) #Sensor 10
-        #GPIO.setup(sensor11, GPIO.IN) #Sensor 11
-        #GPIO.setup(sensor12, GPIO.IN) #Sensor 12
-        #GPIO.setup(sensor13, GPIO.IN) #Sensor 13
-        #GPIO.setup(sensor14, GPIO.IN) #Sensor 14
-        #GPIO.setup(sensor15, GPIO.IN) #Sensor 15
-        #GPIO.setup(sensor16, GPIO.IN) #Sensor 16
+        with open("config.json", "r") as file:
+            config = json.load(file)
+        sensors = {}
+        for x in range(1, 17):
+            sensors[f"sensor{x}"] = config[f"sensor{x}"]
+        for sensor in sensors:
+            GPIO.setup(sensors[sensor], GPIO.IN) #Setup each sensor as an input
     except Exception as e:
         print(e)
 
