9b0a3880c477257cc62095eb7fb758169a2e7c18
Denis Moeller <144907470+NinjaV2Kn@users.noreply.github.com>
2023-12-07 09:20:35 +0100

    removed a little bit of code

--- test_flask/login.py
+++ test_flask/login.py
@@ -3,19 +3,15 @@ import os
 
 app = Flask(__name__)
 
-@app.route("/login/", methods=["GET", "POST"])
+@app.route("/")
 def index():
     """
     Web Page to Display Login Form and process form. 
     """
     if request.method == "POST":
         pw = os.environ.get('pass_api_key')
-        #If we found a user based on username then compare that the submitted
-        #password matches the password in the database.  The password is stored
-        #is a slated hash format, so you must hash the password before comparing
-        #it.
         if request.form['password'] == pw:
-            return redirect(request.args.get("next") or "/")        
+            return redirect(request.args.get("Mate_website.html") or "/")        
 
     return render_template("LoginDashboard.html")
 
