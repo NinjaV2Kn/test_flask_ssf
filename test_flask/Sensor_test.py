f012850908fe3b40612335e58bb7f57434afd8e4
Denis Moeller <144907470+NinjaV2Kn@users.noreply.github.com>
2023-12-07 12:08:01 +0000

    pushed and pulled stuff

--- test_flask/Sensor_test.py
+++ test_flask/Sensor_test.py
@@ -1,21 +1,17 @@
 from flask import Flask, render_template
 import BottleSensors as bs
 import os
-import bottle_counter as gbc
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     sensorSts = bs.bottle_counter()
-    count     = gbc.sold_bottle_count
     templateData = {
         'title': 'GPIO input Status!',
         'button': sensorSts,
-        'quantity': count,
-    
     }
-                         #'Mate_website.html'
+    
     return render_template('index.html', **templateData)
 
 def startFlask():
