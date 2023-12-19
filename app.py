04704167101025fcd417f346bdb3cf750e3fc55a
Denis Moeller <144907470+NinjaV2Kn@users.noreply.github.com>
2023-12-19 10:37:57 +0000

    added the message receiver

--- app.py
+++ app.py
@@ -13,6 +13,46 @@ with open("bottle_count.json", "r") as file:
     data = json.load(file)
     value = int(data['count'])
 
+import time
+from azure.iot.device import IoTHubDeviceClient
+
+RECEIVED_MESSAGES = 0
+bottles: int= 0
+
+CONNECTION_STRING = "HostName=fian23-fridge-hub.azure-devices.net;DeviceId=Raspberry;SharedAccessKey=FnuqGxLtzbBQS1aQXgBE02dR5RTLyOxfhAIoTBgmTKU="
+
+def message_handler(message):
+    global RECEIVED_MESSAGES
+    RECEIVED_MESSAGES += 1
+    print("")
+    print("Message received:")
+
+    # print data from both system and application (custom) properties
+    bottles = vars(message)['custom_properties']['BottleSensors']
+    print(bottles)
+
+    print("Total calls received: {}".format(RECEIVED_MESSAGES))
+
+def mainMessage():
+    print ("Starting the Python IoT Hub C2D Messaging device sample...")
+
+    # Instantiate the client
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+
+    print ("Waiting for C2D messages, press Ctrl-C to exit")
+    try:
+        # Attach the handler to the client
+        client.on_message_received = message_handler
+
+        while True:
+            time.sleep(1000)
+    except KeyboardInterrupt:
+        print("IoT Hub C2D Messaging device sample stopped")
+    finally:
+        # Graceful exit
+        print("Shutting down IoT Hub Client")
+        client.shutdown()    
+
 @app.route("/")
 def index():
     return render_template("login.html")
@@ -45,9 +85,10 @@ def protected():
         with open("bottle_count.json", "r") as file:
             data = json.load(file)
             value = int(data['count'])
+            mainMessage()
 
         if is_logged_in():
-            sensorSts = 12 #bs.bottle_counter()
+            sensorSts = bottles
             count = value
             temper = 5 #tp.TempCalc()
             templateData = {
@@ -56,7 +97,7 @@ def protected():
                 'button': sensorSts,
                 'quantity': count,
             }
-            return render_template("Mate_website.html", **templateData)
+            return render_template("probe.html", **templateData)
         else:
             return redirect(url_for('login'))
     except Exception as e:
