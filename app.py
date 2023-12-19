af296f5a1eed5a0c6bb7a3e4976088dc98cffaba
Denis Moeller <144907470+NinjaV2Kn@users.noreply.github.com>
2023-12-19 10:55:03 +0000

    used the dataReceiver as an external python data

--- app.py
+++ app.py
@@ -14,43 +14,10 @@ with open("bottle_count.json", "r") as file:
 
 import time
 from azure.iot.device import IoTHubDeviceClient
-
-RECEIVED_MESSAGES = 0
-bottles: int= 0
-
-CONNECTION_STRING = "HostName=fian23-fridge-hub.azure-devices.net;DeviceId=Raspberry;SharedAccessKey=FnuqGxLtzbBQS1aQXgBE02dR5RTLyOxfhAIoTBgmTKU="
-
-def message_handler(message):
-    global RECEIVED_MESSAGES
-    RECEIVED_MESSAGES += 1
-    print("")
-    print("Message received:")
-
-    # print data from both system and application (custom) properties
-    bottles = vars(message)['custom_properties']['BottleSensors']
-    print(bottles)
-
-    print("Total calls received: {}".format(RECEIVED_MESSAGES))
-
-def mainMessage():
-    print ("Starting the Python IoT Hub C2D Messaging device sample...")
-
-    # Instantiate the client
-    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
-
-    print ("Waiting for C2D messages, press Ctrl-C to exit")
-    try:
-        # Attach the handler to the client
-        client.on_message_received = message_handler
-
-        while True:
-            time.sleep(1000)
-    except KeyboardInterrupt:
-        print("IoT Hub C2D Messaging device sample stopped")
-    finally:
-        # Graceful exit
-        print("Shutting down IoT Hub Client")
-        client.shutdown()    
+import dataReceived as dr
+  
+dr.main()
+bottles = dr.message_handler()
 
 @app.route("/")
 def index():
--- dataReceived.py
+++ dataReceived.py
@@ -1,22 +1,18 @@
 import time
 from azure.iot.device import IoTHubDeviceClient
 
-RECEIVED_MESSAGES = 0
 bottles: int= 0
 
 CONNECTION_STRING = "HostName=fian23-fridge-hub.azure-devices.net;DeviceId=Raspberry;SharedAccessKey=FnuqGxLtzbBQS1aQXgBE02dR5RTLyOxfhAIoTBgmTKU="
 
-def message_handler(message):
-    global RECEIVED_MESSAGES
-    RECEIVED_MESSAGES += 1
-    print("")
-    print("Message received:")
-
+def message_handler(message) -> int:
     # print data from both system and application (custom) properties
     bottles = vars(message)['custom_properties']['BottleSensors']
     print(bottles)
+    return bottles
 
-    print("Total calls received: {}".format(RECEIVED_MESSAGES))
+    
+
 
 def main():
     print ("Starting the Python IoT Hub C2D Messaging device sample...")
