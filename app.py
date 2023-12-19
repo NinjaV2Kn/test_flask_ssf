43c4eba37eaca4f228c9977c15751f452c421550
Denis Moeller <144907470+NinjaV2Kn@users.noreply.github.com>
2023-12-19 11:18:34 +0000

    removed the receiver

--- app.py
+++ app.py
@@ -12,12 +12,6 @@ with open("bottle_count.json", "r") as file:
     data = json.load(file)
     value = int(data['count'])
 
-
-from azure.iot.device import IoTHubDeviceClient
-import dataReceived as dr
-
-bottles = dr.message_handler()
-
 @app.route("/")
 def index():
     return render_template("login.html")
