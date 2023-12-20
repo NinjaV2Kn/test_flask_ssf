2e98793e1dc16a2fa5ec3b556e11aab7eb2dd454
Denis Moeller <144907470+NinjaV2Kn@users.noreply.github.com>
2023-12-20 07:56:07 +0000

    added the message reader in the app.py

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ import os
 import base64
 import time
 import json
+from azure.iot.device import IoTHubDeviceClient
+
+CONNECTION_STRING = "HostName=fian23-fridge-hub.azure-devices.net;DeviceId=Raspberry;SharedAccessKey=FnuqGxLtzbBQS1aQXgBE02dR5RTLyOxfhAIoTBgmTKU="
 
 app = Flask(__name__)
 app.secret_key = 'FIAN23!de'
 
+client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
+
+
+
+
 @app.route("/")
 def index():
-    return render_template("login.html")
+    return redirect(url_for('login'))
 
 # This function checks if the user is logged in
 def is_logged_in():
@@ -35,12 +43,28 @@ def login():
 # Route for the protected page
 @app.route('/Mate')
 def protected():
+
+    def message_handler(message) -> int:
+        print("started message_handler")
+        # print data from both system and application (custom) properties
+        temp = vars(message)['custom_properties']['temp']
+        bottles = vars(message)['custom_properties']['bottles']
+        count = vars(message)['custom_properties']['count']
+        print("loaded props")
+        with open("bottle_count.json", "w") as file:
+            json.dump({"bottles": bottles, "temp": temp, "count": count}, file)
+        print("dumped the message")
+
     try:
+        client.on_message_received = message_handler
+        print("loaded on_message_received")
         with open("bottle_count.json", "r") as file: #opens the json to get the data
+            print("loaded json file")
             data = json.load(file)
             value = int(data['count'])
             bottles = int(data['bottles'])
             temperature = int(data['temp'])
+            print("loaded data from json")
 
 
         if is_logged_in():
@@ -53,6 +77,7 @@ def protected():
                 'button': sensorSts,
                 'quantity': count,
             }
+            print("loaded is_logged_in")
             return render_template("probe.html", **templateData)
         else:
             return redirect(url_for('login'))
@@ -65,8 +90,8 @@ def protected():
 def logout():
     session['logged_in'] = False
     return redirect(url_for('login'))
-#def start() -> None:
- #   app.run(host="192.168.30.154", port="5010", debug=True)
+# def start() -> None:    
+#     app.run(host="192.168.30.32", port="5010", debug=True)
 
 if __name__ == "__main__":
     app.run()
